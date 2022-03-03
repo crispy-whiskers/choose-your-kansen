9ba788b4b907f6142e18ef187c1c79756123027c
ladsfdsfs <nguyen.k.allen@gmail.com>
2022-03-03 16:02:05 -0500

    working image loader

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { createCanvas, loadImage } = require('canvas')
 const fs = require("fs");
-const WIDTH = 1275;
-const HEIGHT = 638;
+const WIDTH = 1920;
+const HEIGHT = 1080;
 const canvas = createCanvas(WIDTH, HEIGHT); //createimage?
 
 const ctx = canvas.getContext("2d");
 
 
-loadImage("./assets/img/background.png").then((image) => {
+loadImage("./assets/img/background1.jpg").then((image) => {
     ctx.drawImage(image, 0, 0);
     const buffer = canvas.toBuffer("image/png");
     fs.writeFileSync("./image.png", buffer);
