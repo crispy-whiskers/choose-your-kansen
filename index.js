1bde3aac5f2e05788c0006bd1b030d9ef632a12d
ladsfdsfs <nguyen.k.allen@gmail.com>
2022-03-03 21:09:22 -0500

    now load images from json

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { createCanvas, loadImage } = require('canvas')
+const { AzurAPI } = require("@azurapi/azurapi");
+const kansens = require('./assets/data/ships.json')
+const assets = "./assets/img"
+
 const fs = require("fs");
 const WIDTH = 1920;
 const HEIGHT = 1080;
@@ -6,11 +10,29 @@ const canvas = createCanvas(WIDTH, HEIGHT); //createimage?
 
 const ctx = canvas.getContext("2d");
 
+const azurlane = new AzurAPI();
 
-loadImage("./assets/img/background1.jpg").then((image) => {
-    ctx.drawImage(image, 0, 0);
-    const buffer = canvas.toBuffer("image/png");
-    fs.writeFileSync("./image.png", buffer);
-  });
+let kansen = kansens[156]
+const avatarURL = kansen.skins[0].chibi;
+console.log(avatarURL);
+//exports.run = async (client, message, args, ops) => {
+
+
+(async () => {
+//should use path library here but im lazy af
+  let image = await loadImage(assets+"/background1.jpg");
+  ctx.drawImage(image, 0, 0);
+
+  let someShip = await loadImage(avatarURL);
+  ctx.drawImage(someShip, 200, 200);
+
+  const buffer = canvas.toBuffer("image/png");
+  fs.writeFileSync("./test/image.png", buffer);
+})().then(()=>{
+  console.log('done')
+  sys.exit(0)
+});
+
   
+
 
